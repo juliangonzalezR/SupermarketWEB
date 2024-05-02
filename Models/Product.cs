b35ca92d9d1e641decf3c61cbb5a1066168d6520
JulianGonzalez <juliangonnza1234@gmail.com>
2024-05-02 10:23:19 -0500

    Index Products

--- Models/Product.cs
+++ Models/Product.cs
@@ -7,10 +7,7 @@ namespace SupermarketWEB.Models
     {
         public int Id { get; set; }
         public string Name { get; set; }
-        [Column(TypeName = "decimal(6,2)")]
-        public decimal Price { get; set; }
+        public int Price { get; set; }
         public int Stock { get; set; }
-        public int CategoryId { get; set; }
-        public Category Category { get; set; }
     }
 }
